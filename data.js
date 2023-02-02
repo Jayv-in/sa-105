7797053738cfe2f38f8884946e5aec14c549108d
Jayv-in <124267738+Jayv-in@users.noreply.github.com>
2023-02-02 14:12:12 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,21 @@
+var numCase = 0;
+
+var data = [
+
+{"analogie":"un élément", "valeuranalogie":"LA GLACE", "descriptionanalogie" : "Parce que je peux paraître froid et distant. Ma timidité m'isole un peu du reste et parfois la présence des autres me dérange.", "imagesanalogie" : "images/glace.jpg"},
+
+{"analogie":"un sentiment", "valeuranalogie":"L'AMOUR", "descriptionanalogie" : "Parce que je suis quelqu’un de romantique et sentimental qui n’a pas peur de déclarer ses sentiments à ses proches ", "imagesanalogie" : "images/amoureux.jpg"},
+
+{"analogie":"un super-pouvoir", "valeuranalogie":"INVISIBLE", "descriptionanalogie" : "Parce que j’aime passer inaperçue et ça serais une façon totalement différente de voir la vie de cette manière.", "imagesanalogie" : "images/super-pouvoir.jpg"},
+
+{"analogie":"un animal", "valeuranalogie":"LE PARESSEUX", "descriptionanalogie" : "Parce que je suis quelqu’un de nonchalant et j’ai du mal à me motiver et me mettre au travail ", "imagesanalogie" : "images/paresseux.jpg"},
+
+{"analogie":"une arme", "valeuranalogie":"L'ARC", "descriptionanalogie" : "Parce que c’est une arme particulière qui est plutôt compliqué à manier et c’est une arme à distance qui évite le corps à corps , et je suis quelqu’un qui évite le contact.", "imagesanalogie" : "images/arc.webp"},
+
+{"analogie":"une couleur", "valeuranalogie":"LE VIOLET", "descriptionanalogie" : "Parce qu’elle représente le rêve , la méditation ou encore la délicatesse et dans le langage des fleurs le violet est le symbole de l’amour caché. ", "imagesanalogie" : "images/violet1.jpg"},
+
+{"analogie":"un héro de la mythologie grecque", "valeuranalogie":"ORPHÉE", "descriptionanalogie" : "Parce qu’il a été comblé de dons par Apollon, il est très sensible et pour l'amour d'Eurydice, sa femme, va jusqu'à descendre aux Enfers pour sauver sa bien-aimée ce qui me ressemble.", "imagesanalogie" : "images/Orphée.jpg"},
+
+
+
+];
